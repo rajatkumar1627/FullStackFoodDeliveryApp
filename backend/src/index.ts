253e7bba46d2b7bd75e66b70196f12187bd2a771
Rajat Jha <rajat.kumar319@gmail.com>
2024-02-19 16:54:32 +0530

    Modified the database connection as per typescript

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,9 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(()=> console.log("Connected to database!"));
 
 const app = express();
 app.use(express.json());
