e290c6241297c3564a7125c2197d8cc13cc7161b
Rajat Jha <rajat.kumar319@gmail.com>
2024-02-20 16:46:35 +0530

    Added the login logout functionalities for mobile as well as big screen and also added authentication based links

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,25 +1,50 @@
-import { Menu } from "lucide-react";
-import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { CircleUserRound, Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = () => {
-  return(
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+  return (
     <Sheet>
       <SheetTrigger>
-        <Menu className="text-orange-500"/>
+        <Menu className="text-orange-500" />
       </SheetTrigger>
       <SheetContent className="space-y-3">
         <SheetTitle>
-          <span>Welcome to PatnaEats.com</span>
+          {isAuthenticated ? (
+            <span className="flexitems-center font-bold gap-2">
+              <CircleUserRound className="text-green-500" />
+              {user?.email}
+            </span>
+          ) : (
+            <span>Welcome to PatnaEats.com</span>
+          )}
         </SheetTitle>
-        <Separator/>
-        <SheetDescription className="flex">
-          <Button className="flex-1 font-bold bg-green-500">Log In</Button>
+        <Separator />
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              onClick={() => loginWithRedirect()}
+              className="flex-1 font-bold bg-green-500"
+            >
+              Log In
+            </Button>
+          )}
         </SheetDescription>
       </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
 
 export default MobileNav;
