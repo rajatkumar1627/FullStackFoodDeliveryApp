41c0be219ff58686d811e6a2a657103b12a0050e
Rajat Jha <rajat.kumar319@gmail.com>
2024-02-20 12:35:23 +0530

    added the mobile nav component also added separator and sheets using shadcn

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return(
@@ -7,6 +8,9 @@ const Header = () => {
         <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500">
           PatnaEats.com
         </Link>
+        <div className="md:hidden">
+          <MobileNav/>
+        </div>
       </div>
     </div>
   )
